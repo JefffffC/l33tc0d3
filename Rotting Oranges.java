51b583df81fc387d4bca802611fa2d4935fa0da3
Jeffrey Chen <44081961+JefffffC@users.noreply.github.com>
2019-06-18 11:35:21 -0400

    changed to BFS solution
    faster than 47%, space complexity better than 99%.

--- Rotting Oranges.java
+++ Rotting Oranges.java
@@ -1,62 +1,80 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
+        int minutes = 0;
+        Queue<Integer> queue = new ArrayDeque<Integer>();
+        HashMap<Integer,Integer> gen = new HashMap<Integer,Integer>();
         
-        boolean isDone = false;
-        int numMinutes = 0;
+        int numCol = grid[0].length;
+        int numRow = grid.length;
+        int numOranges = 0;
         
+        for (int r = 0; r < numRow; r++) {
+            for (int c = 0; c < numCol; c++) {
+                if (grid[r][c] == 2) { // if orange is rotting, enqueue cell position
+                    int code = getCode(r, c, numCol);
+                    queue.add(code);
+                    gen.put(code, 0); // add cell position and depth (zero for start)
+                    numOranges++;
+                }
+                if (grid[r][c] == 1)
+                    numOranges++;
+            }
+        }
         
-        int i = 0;
-        int j = 0;
-        while (i < 1) {
-            isDone = true;
-            for (int x = 0; x < grid.length; x++) {
-                for (int y = 0; y < grid[x].length; y++) {
-                    if (rotAdjacent(grid, x, y) == true) {
-                        isDone = false;
-                        j++;
+        int numRotted = 0;
+        
+        while (!queue.isEmpty()) {
+            int code = queue.remove();
+            numRotted++;
+            int c = code % numCol;
+            int r = code / numCol;
+            if (grid[r][c] == 2) {
+                if (r > 0) {
+                    if (grid[r - 1][c] == 1) {
+                        grid[r - 1][c] = 2;
+                        int newCode = getCode(r - 1, c, numCol); // get the 1D representation of child cell
+                        queue.add(newCode); // enqueue child cell
+                        gen.put(newCode, gen.get(code) + 1); // increase the gen (minute) of the child cell
+                        minutes = gen.get(newCode); // increase minutes to latest one
                     }
                 }
+                if (r + 1 < numRow) {
+                    if (grid[r + 1][c] == 1) {
+                        grid[r + 1][c] = 2;
+                        int newCode = getCode(r + 1, c, numCol); // get the 1D representation of child cell
+                        queue.add(newCode); // enqueue child cell
+                        gen.put(newCode, gen.get(code) + 1); // increase the gen (minute) of the child cell
+                        minutes = gen.get(newCode);
+                    }   
+                }
+                if (c > 0) {
+                    if (grid[r][c - 1] == 1) {
+                        grid[r][c - 1] = 2;
+                        int newCode = getCode(r, c - 1, numCol); // get the 1D representation of child cell
+                        queue.add(newCode); // enqueue child cell
+                        gen.put(newCode, gen.get(code) + 1); // increase the gen (minute) of the child cell
+                        minutes = gen.get(newCode);
+                    }
+                }
+                if (c + 1 < numCol) {
+                    if (grid[r][c + 1] == 1) {
+                        grid[r][c + 1] = 2;
+                        int newCode = getCode(r, c + 1, numCol); // get the 1D representation of child cell
+                        queue.add(newCode); // enqueue child cell
+                        gen.put(newCode, gen.get(code) + 1); // increase the gen (minute) of the child cell
+                        minutes = gen.get(newCode);
+                    }
+                }   
             }
-            if (isDone = false) {
-                numMinutes++;
-            }
-            i++;
         }
-        return j;
+        
+        if (numRotted != numOranges)
+            return -1;
+        
+        return minutes;
     }
     
-    private boolean rotAdjacent(int[][] grid, int x, int y) {
-        
-        boolean didRot = false;
-        
-        if (grid[x][y] == 2) {
-            if (x > 0) {
-                if (grid[x - 1][y] == 1) {
-                    grid[x - 1][y] = 2;
-                    didRot = true;
-                }
-            }
-            if (x + 1 < grid.length) {
-                if (grid[x + 1][y] == 1) {
-                    grid[x + 1][y] = 2;
-                    didRot = true;
-                }
-            }
-            if (y > 0) {
-                if (grid[x][y - 1] == 1) {
-                    grid[x][y - 1] = 2;
-                    didRot = true;
-                }
-            }
-            if (y + 1 < grid[0].length) {
-                if (grid[x][y + 1] == 1) {
-                    grid[x][y + 1] = 2;
-                    didRot = true;
-                }
-            }   
-        }
-        else didRot = false;
-        
-        return didRot;
+    private int getCode(int r, int c, int numCol) {
+        return (r * numCol) + c;
     }
 }
