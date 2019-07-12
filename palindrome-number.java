52e2405785911b421fb2d04a7b68931adfc7b6f1
Jeffrey Chen <44081961+JefffffC@users.noreply.github.com>
2019-07-12 11:41:59 -0400

    Create palindrome-number.java
    O(n) TC, will seek to improve.

---
+++ palindrome-number.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) return false;
+        else {
+            int sz = 0;
+            ArrayDeque<Integer> stack = new ArrayDeque<>();
+            while (x > 0) {
+                stack.push(x % 10);
+                x = x / 10;
+                sz++;
+            }
+            int[] arr = new int[sz];
+            for (int i = 0; i < sz; i++) {
+                arr[i] = stack.pop();
+            }
+            int lptr = 0;
+            int rptr= arr.length - 1;
+            while (lptr < rptr) {
+                if (arr[lptr] != arr[rptr])
+                    return false;
+                lptr ++;
+                rptr --;
+            }
+            return true;
+        }
+    }
+}
